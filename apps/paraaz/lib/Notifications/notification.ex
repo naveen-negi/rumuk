d33b378f3a01db0bd3969dc2bcedfb00e1b786a0
Naveen Negi <naveen@naveens-MacBook-Pro.local>
2017-02-20 21:23:04 +0530

    adding message notification

--- apps/paraaz/lib/Notifications/notification.ex
+++ apps/paraaz/lib/Notifications/notification.ex
@@ -3,24 +3,12 @@ defmodule Paraaz.Notification do
  alias Riak.CRDT.Register   
  alias Riak.CRDT.Set  
 
-    # def new(belongs_to, type, id, category_fields) do
-    #     user = Register.new(belongs_to)
-    #     category = Register.new(type)
-    #     id = Register.new(id)
-
-    #     Map.new
-    #     |>Map.put("user-id", user) 
-    #     |>Map.put("category-type", category)
-    #     |>Map.put("notification-id", id)
-    #     |>Map.put("category-fields", category_fields)
-    # end
-
     def new(:declined, belongs_to, category_type, category_fields) do
         
-         user_id = Register.new(belongs_to)
+        user_id = Register.new(belongs_to)
         category = Register.new(category_type)
         
-        category_fields = create_category_fields(:declined, category_fields)
+        category_fields = create_category_fields(category_fields)
         
         id_raw = belongs_to <> category_type <> to_string(:os.system_time(:seconds))
 
@@ -35,9 +23,58 @@ defmodule Paraaz.Notification do
         %{id: id_raw, notification: map}
     end
 
-    defp create_category_fields(:declined, category_fields) do
+    def new(:invitation_received, belongs_to, category_type, category_fields) do
+            
+            user_id = Register.new(belongs_to)
+            category = Register.new(category_type)
+            
+            category_fields = create_category_fields(category_fields)
+            
+            id_raw = belongs_to <> category_type <> to_string(:os.system_time(:seconds))
+
+            id = Register.new(id_raw)
+
+            map = Map.new
+                    |>Map.put("user-id", user_id) 
+                    |>Map.put("category-type", category)
+                    |>Map.put("notification-id", id)
+                    |>Map.put("category-fields", category_fields)
+
+            %{id: id_raw, notification: map}
+        end
+
+    def new(:message_received, belongs_to, category_type, category_fields) do
+                
+                user_id = Register.new(belongs_to)
+                category = Register.new(category_type)
+                
+                category_fields = create_category_fields(:message_received, category_fields)
+                
+                id_raw = belongs_to <> category_type <> to_string(:os.system_time(:seconds))
+
+                id = Register.new(id_raw)
+
+                map = Map.new
+                        |>Map.put("user-id", user_id) 
+                        |>Map.put("category-type", category)
+                        |>Map.put("notification-id", id)
+                        |>Map.put("category-fields", category_fields)
+
+                %{id: id_raw, notification: map}
+    end
+
+    defp create_category_fields(category_fields) do
         sender = Register.new(category_fields.sender)
         Map.new 
-        |> Map.put("sender", sender)
+        |> Map.put("sender-id", sender)
+    end
+
+    defp create_category_fields(:message_received, category_fields) do
+        sender = Register.new(category_fields.sender)
+        content = Register.new(category_fields.content)
+
+        Map.new 
+        |> Map.put("sender-id", sender)
+        |> Map.put("content", content)
     end
 end
--- apps/paraaz/test/notifications_test.exs
+++ apps/paraaz/test/notifications_test.exs
@@ -90,9 +90,41 @@ end
     category_type  = "invitation_declined"
    
     sender = "sender_id_1"
-    category_fields = %{sender: "user_1"}
+    category_fields = %{sender: sender}
 
     %{id: id, notification: notification} = Paraaz.Notification.new(:declined, belongs_to, category_type, category_fields)
+
+    notification |> Riak.update("maps", "notifications", id)
+
+    map = Riak.find("maps", "notifications", id) |> Map.value
+    
+     assert :orddict.size(map) == 4
+     map_keys = :orddict.fetch_keys(map)
+     validate_structure(map_keys)
+
+      data = :orddict.to_list(map)
+      
+      assert {{"user-id", :register}, "naveen"} in data
+      assert {{"category-type", :register}, category_type} in data
+      assert {{"notification-id", :register}, id} in data
+      
+        nested_map = get_nested_map("category-fields", map)
+
+        sender_id = :orddict.fetch({"sender-id", :register}, nested_map)
+
+        assert sender_id == "sender_id_1"
+  end
+
+
+  test "should be able to create and save invitation received notification" do
+    belongs_to = "user123"
+    category_type = "invitation_received"
+
+    sender = "sender_id_1"
+
+    category_fields = %{sender: "sender_1"}
+
+     %{id: id, notification: notification} = Paraaz.Notification.new(:invitation_received, belongs_to, category_type, category_fields)
 
     notification |> Riak.update("maps", "notifications", id)
 
@@ -101,6 +133,35 @@ end
     IO.inspect map
 
      assert :orddict.size(map) == 4
+  end
+
+  test "should be able to create and save a message received notification" do
+    
+    belongs_to = "user123"
+    category_type = "invitation_received"
+    sender = "sender_id_1"
+    content = "Hello there ! how are you"
+
+    category_fields = %{sender: sender, content: content}
+     %{id: id, notification: notification} = Paraaz.Notification.new(:message_received, belongs_to, category_type, category_fields)
+    notification |> Riak.update("maps", "notifications", id)
+
+    map = Riak.find("maps", "notifications", id) |> Map.value
+
+     assert :orddict.size(map) == 4
+     map_keys = :orddict.fetch_keys(map)
+     validate_structure(map_keys)
+
+      data = :orddict.to_list(map)
+      
+      assert {{"user-id", :register}, belongs_to} in data
+      assert {{"category-type", :register}, category_type} in data
+      assert {{"notification-id", :register}, id} in data
+      
+        nested_map = get_nested_map("category-fields", map)
+
+        assert :orddict.fetch({"sender-id", :register}, nested_map) == sender
+        assert :orddict.fetch({"content", :register}, nested_map) == content
   end
 
   test "basic" do
@@ -114,5 +175,17 @@ end
       map = Riak.find("maps", "my_map_bucket", "map_key") |> Map.value
       # IO.inspect map
       assert map != nil
+  end
+
+  defp validate_structure(map_keys) do
+    
+      assert {"user-id", :register} in map_keys
+      assert {"category-type", :register} in map_keys
+      assert {"notification-id", :register} in map_keys
+      assert {"category-fields", :map} in map_keys
+
+  end
+  defp get_nested_map(key, map) do
+    :orddict.fetch({key, :map}, map)
   end
 end
