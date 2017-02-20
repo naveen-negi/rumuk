a01eecf9e113be43e43d0fa8231c18b2911e8e41
Naveen Negi <naveen@naveens-MacBook-Pro.local>
2017-02-20 20:45:06 +0530

    all test passing

--- apps/paraaz/lib/Notifications/notification.ex
+++ apps/paraaz/lib/Notifications/notification.ex
@@ -1,14 +1,43 @@
 defmodule Paraaz.Notification do
  alias Riak.CRDT.Map   
  alias Riak.CRDT.Register   
+ alias Riak.CRDT.Set  
 
-    def new(belongs_to, type) do
-        user = Register.new(belongs_to)
+    # def new(belongs_to, type, id, category_fields) do
+    #     user = Register.new(belongs_to)
+    #     category = Register.new(type)
+    #     id = Register.new(id)
+
+    #     Map.new
+    #     |>Map.put("user-id", user) 
+    #     |>Map.put("category-type", category)
+    #     |>Map.put("notification-id", id)
+    #     |>Map.put("category-fields", category_fields)
+    # end
+
+    def new(:declined, belongs_to, category_type, category_fields) do
         
-        category = Register.new(type)
+         user_id = Register.new(belongs_to)
+        category = Register.new(category_type)
+        
+        category_fields = create_category_fields(:declined, category_fields)
+        
+        id_raw = belongs_to <> category_type <> to_string(:os.system_time(:seconds))
 
-        Map.new
-        |>Map.put("user", user) 
-        |>Map.put("category", category)
+        id = Register.new(id_raw)
+
+        map = Map.new
+                |>Map.put("user-id", user_id) 
+                |>Map.put("category-type", category)
+                |>Map.put("notification-id", id)
+                |>Map.put("category-fields", category_fields)
+
+        %{id: id_raw, notification: map}
+    end
+
+    defp create_category_fields(:declined, category_fields) do
+        sender = Register.new(category_fields.sender)
+        Map.new 
+        |> Map.put("sender", sender)
     end
 end
--- apps/paraaz/test/notifications_test.exs
+++ apps/paraaz/test/notifications_test.exs
@@ -3,37 +3,104 @@ defmodule NotificationTest do
   alias Riak.CRDT.Map
   alias Riak.CRDT.Register
   alias Riak.CRDT.Flag
-
   doctest Paraaz
 
-  test "should be able to create and save a notification" do
-    belongs_to = "naveen"
-    type = "request_received"
-    id = belongs_to <> "_" <> type <> "_3";
+  # test "should be able to save  and retrieve a notification" do
+  #   belongs_to = "naveen"
+  #   type = "request_received"
+  #   id = belongs_to <> "_" <> type <> "_3";
+  #   sender = "sender_id_1"
+  #   category = Paraaz.InvitationCategory.new(sender)
+
+  #   notification = Paraaz.Notification.new(belongs_to, type, id, category)
     
-    notification = Paraaz.Notification.new(belongs_to, type)
-    # %{id: value} = notification
-    # Map.new
-    # |> Map.put("notification", notification)
-     notification |> Riak.update("maps", "notifications", id)
+    
+    
+  #   notification |> Riak.update("maps", "notifications", id, category)
+
+  #   map = Riak.find("maps", "notifications", id) |> Map.value
+    
+  #   IO.inspect map
+  #   assert :orddict.size(map) == 3
+  
+  # map_keys = :orddict.fetch_keys(map)
+  # IO.puts "keys"
+  # IO.inspect map_keys
+  #   assert {"user", :register} in map_keys
+  #   assert {"category", :register} in map_keys
+  #   assert {"notification-id", :register} in map_keys
+
+  #   data = :orddict.to_list(map)
+  #   IO.puts "data"
+  #   IO.inspect data
+  #   assert {{"user", :register}, "naveen"} in data
+  #   assert {{"category", :register}, "request_received"} in data
+  #   assert {{"notification-id", :register}, id} in data
+  # end
+
+defp clean_up(id) do
+Riak.delete("maps", "notifications", id)
+end
+
+  # test "should contain category fields for for invitation request" do
+  #   belongs_to = "naveen"
+  #   type = "invitation_request"
+  #   id = belongs_to <> "_" <> type <> "_4";
+  #   clean_up(id)
+   
+  #   sender = "sender_id_1"
+  #   category = Paraaz.InvitationCategory.new(sender)
+  #   IO.inspect category
+
+  #   notification = Paraaz.Notification.new(belongs_to, type, id, category)
+
+  #  notification |> Riak.update("maps", "notifications", id)
+
+  #   map = Riak.find("maps", "notifications", id) |> Map.value
+    
+  #   IO.inspect map
+  #   assert :orddict.size(map) == 4
+  
+  # map_keys = :orddict.fetch_keys(map)
+  
+  # IO.puts "keys"
+  # IO.inspect map_keys
+  # IO.puts "==========================================="
+  #   assert {"user-id", :register} in map_keys
+  #   assert {"category-type", :register} in map_keys
+  #   assert {"notification-id", :register} in map_keys
+  #   assert {"category-fields", :map} in map_keys
+
+  #   data = :orddict.to_list(map)
+  #   IO.puts "data"
+  #   IO.inspect data
+  #   IO.puts "==========================================="
+  #   assert {{"user-id", :register}, "naveen"} in data
+  #   assert {{"category-type", :register}, type} in data
+  #   assert {{"notification-id", :register}, id} in data
+    
+  #     nested_map = :orddict.fetch({"category-fields", :map}, map)
+  #     sender_id = :orddict.fetch({"sender-id", :register}, nested_map)
+
+  #     assert sender_id == "sender_id_1"
+  # end
+
+  test "should create and save request declined notification" do
+     belongs_to = "naveen"
+    category_type  = "invitation_declined"
+   
+    sender = "sender_id_1"
+    category_fields = %{sender: "user_1"}
+
+    %{id: id, notification: notification} = Paraaz.Notification.new(:declined, belongs_to, category_type, category_fields)
+
+    notification |> Riak.update("maps", "notifications", id)
 
     map = Riak.find("maps", "notifications", id) |> Map.value
     
     IO.inspect map
-    assert :orddict.size(map) == 2
-  
-  map_keys = :orddict.fetch_keys(map)
-  IO.puts "keys"
-  IO.inspect map_keys
-    assert {"user", :register} in map_keys
-    assert {"category", :register} in map_keys
 
-
-    data = :orddict.to_list(map)
-    IO.puts "data"
-    IO.inspect data
-    assert {{"user", :register}, "naveen"} in data
-    assert {{"category", :register}, "request_received"} in data
+     assert :orddict.size(map) == 4
   end
 
   test "basic" do
