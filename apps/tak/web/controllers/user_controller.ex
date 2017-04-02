b340310e70d8da1f35b048aec89651eb3d1320a9
naveen <nav301186@gmail.com>
2017-04-02 07:28:07 +0530

    adding user controller

--- apps/tak/web/controllers/user_controller.ex
+++ apps/tak/web/controllers/user_controller.ex
@@ -3,12 +3,21 @@ defmodule Tak.UserController do
     alias Tak.User
     alias Tak.User.{BasicInfo, EducationalDetails}
 
-    def get_basic_info(conn, _params) do
+
+    def create_basic_info(conn, _params) do
         user_id = conn.params["user_id"]
-                     basic_info = Tak.UserServer.lookup(self(), user_id)
+        changeset = BasicInfo.changeset(%BasicInfo{}, _params)
+      
+        case changeset.valid? do
+            true -> User.new(user_id) 
+                    |> User.update(changeset.data)
+                    |> Tak.UserServer.save(self())
+                     send_resp(conn, 204,"")
+            false ->  errors = Tak.ChangesetView.render("error.json", %{changeset: changeset})
                       conn
                       |> put_resp_content_type("application/json")
-                      |> send_resp(200, Poison.encode!(basic_info))
+                      |> send_resp(400, Poison.encode!(errors))
+        end
     end
 
     def create_educational_details(conn, _params) do
