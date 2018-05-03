31ce86ec2360503442ac4e8606250617a1604938
NAVEEN <nav3011186@gmail.com>
2018-05-03 19:56:01 +0200

    removing file dir after test completion

--- apps/tak/test/controllers/image_controller_test.exs
+++ apps/tak/test/controllers/image_controller_test.exs
@@ -3,7 +3,15 @@ defmodule Tak.ImageControllerTest do
   alias Tak.ImageService
   require Plug
  @moduletag :images
+
   import Mock
+
+  setup_all do
+    on_exit fn ->
+      File.rm_rf!(Path.join(File.cwd!, "test-media-dir"))
+    end
+  end
+
 
     test "should be able to save image for given user" do
       user_id = Tak.Helper.random_key
@@ -12,5 +20,13 @@ defmodule Tak.ImageControllerTest do
       conn = conn() |> post("api/users/#{user_id}/images", %{:image => upload})
       assert conn.status == 201
       end
+    end
+
+    test "should be able to save user image" do
+      user_id = Tak.Helper.random_key
+      image_id = "image.jpg"
+      upload = %Plug.Upload{path: "test/fixtures/image.jpg", filename: "image.jpg"}
+      conn = conn() |> post("api/users/#{user_id}/images", %{:image => upload})
+    end
 
 end
